8d85b81e6cf6e41945978f7c65d36afee0e28827
Erling Mårtensson <erling.martensson@gmail.com>
2021-06-02 19:44:13 +0200

    feature: Basic Nav and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from './Home'
+import AddQuestion from './AddQuestion'
+import ViewQuestion from './ViewQuestion'
+import Leaderboard from './Leaderboard'
+import Login from './Login'
+import Nav from './Nav'
 
 function App() {
   return (
-    <div className="App">App
-    </div>
+    <Router>
+      <div className="container">
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/add" component={AddQuestion} />
+        <Route exact path="/questions/:questionId" component={ViewQuestion} />
+        <Route exact path="/leaderboard" component={Leaderboard} />
+      </div>
+    </Router>
   );
 }
 
