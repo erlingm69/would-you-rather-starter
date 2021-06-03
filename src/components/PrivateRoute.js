37722a0f036e25fca8d99db8031862c86d2e53ba
Erling Mårtensson <erling.martensson@gmail.com>
2021-06-03 11:18:42 +0200

    refactor: Use "" as default

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component, authedUser, ...rest }) => {
     return (
 
         // Show the component only when the user is logged in
         // Otherwise, redirect the user to /signin page
         <Route {...rest} render={props => {
             return (
-            rest.authedUser === null ?
+            authedUser === "" ?
                 <Redirect to={`/login?next=${props.location.pathname}`} />
                 : <Component {...props} />
         )}} />
