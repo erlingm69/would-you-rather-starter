16e8b9f79ef0a0b38ccc6658e6deccca1059c481
Erling Mårtensson <erling.martensson@gmail.com>
2021-06-04 14:36:14 +0200

    feature: In poll results the option selected by the user is shown

--- src/components/PollResults.js
+++ src/components/PollResults.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-function PollResults({ questions, questionId }) {
+function PollResults({ questions, authedUser, questionId }) {
 
     const question = questions[questionId];
     const optionOneAnswers = question.optionOne.votes.length
@@ -9,13 +9,19 @@ function PollResults({ questions, questionId }) {
     const totalAnswers = optionOneAnswers + optionTwoAnswers
     const percentageOptionOne = optionOneAnswers/totalAnswers*100
     const percentageOptionTwo = optionTwoAnswers/totalAnswers*100
+    const userSelectedOptionOne = question.optionOne.votes.includes(authedUser)
+    const userSelectedOptionTwo = question.optionTwo.votes.includes(authedUser)
 
     return (
         <div>
             <div className="question-item">
-                <div className="poll-option">{question.optionOne.text}</div>
+                <div className="poll-option"
+                style={userSelectedOptionOne ? { borderStyle: "dotted" } : null}
+                >{question.optionOne.text}</div>
                 <div className="bold-and-margin">OR</div>
-                <div className="poll-option">{question.optionTwo.text}</div>
+                <div className="poll-option"
+                style={userSelectedOptionTwo ? { borderStyle: "dotted"} : null}
+                >{question.optionTwo.text}</div>
             </div>
             <div className="question-item">
                 <div className="poll-option"><i>{`${percentageOptionOne}% ${optionOneAnswers} vote(s)`}</i></div>
@@ -25,9 +31,10 @@ function PollResults({ questions, questionId }) {
     )
 }
 
-function mapStateToProps({ questions }, { questionId }) {
+function mapStateToProps({ questions, authedUser }, { questionId }) {
     return {
         questions,
+        authedUser,
         questionId
     }
 }
